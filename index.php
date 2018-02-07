42a506414a32b6ad6e7909622e35bdad3f3b0e0b
Mico <micropizza.mc@gmail.com>
2018-02-07 20:40:12 +0200

    WIP Navbar & PHP Function
    Navbar work & php function for calling a random quote/text from the database

--- index.php
+++ index.php
@@ -1,19 +1,9 @@
 <?php
+// PHP Connection requirement
 require('php/utility.php');
-$db = getDbObject();
-$sql = "SELECT texts FROM texts WHERE id=1";
-
-$result = $db->query($sql);
-
-$ilmoittautuneet = array();
-while ($row = $result->fetch()) {
-    $ilmoittautuneet[] = $row;
-}
-var_dump($ilmoittautuneet);
-foreach ($ilmoittautuneet as $rivi) {
-        extract($rivi);
-        echo $texts;
-};
+// PHP Random Text function requirement
+// Call with randomText();
+require('php/randomText.php');
 ?>
 <!DOCTYPE html>
 <html>
@@ -26,24 +16,51 @@ foreach ($ilmoittautuneet as $rivi) {
   </head>
   <body>
     <!--Navigation Bar-->
-    <nav>
-      <ul>
-        <a href="#"><li>Home</li></a>
-        <a href="#"><li>Home</li></a>
-        <a href="#"><li>Home</li></a>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Cat Racer</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Info</a>
+        </li>
+        <?php
+        if (true == true) {
+            echo '<li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Profile
+              </a>
+              <div class="dropdown-menu" role="menu" aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item text-danger" href="#">Logout</a>
+              </div>
+            </li>';
+        }
+        ?>
       </ul>
-    </nav>
+      <span class="navbar-text">
+      Login
+    </span>
+    </div>
+  </nav>
     <!--Header-->
     <header>
-      <?php
-
-      ?>
     </header>
     <!--Main content after this-->
     <div>
 
     </div>
-  </body>
-  <script src="/js/master.js" charset="utf-8"></script>
+    <script
+  src="http://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="/js/master.js" charset="utf-8"></script>
+  </body>
 </html>
