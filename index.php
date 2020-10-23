a98f1274893b457a627c3ba135273ce1cc4e0d77
Mico <micropizza.mc@gmail.com>
2020-10-23 04:31:40 +0300

    Game functionality
    Able to type stuff with right/wrong input display atnd stuff

--- index.php
+++ index.php
@@ -1,66 +1,61 @@
 <?php
-// PHP Connection requirement
-require('php/utility.php');
-// PHP Random Text function requirement
-// Call with randomText();
-require('php/randomText.php');
-?>
+    // PHP Connection requirement
+    require('php/utility.php');
+    $results = $CatText->textQuery();
+    ?>
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>Cat Racer</title>
-    <link rel="stylesheet" href="/css/reset.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="/css/master.css">
-  </head>
-  <body>
-    <!--Navigation Bar-->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Cat Racer</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Info</a>
-        </li>
-        <?php
-        if (true == true) {
-            echo '<li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Profile
-              </a>
-              <div class="dropdown-menu" role="menu" aria-labelledby="navbarDropdownMenuLink">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <a class="dropdown-item text-danger" href="#">Logout</a>
-              </div>
-            </li>';
-        }
-        ?>
-      </ul>
-      <span class="navbar-text">
-      Login
-    </span>
-    </div>
-  </nav>
-    <!--Header-->
-    <header>
-    </header>
-    <!--Main content after this-->
-    <div>
-
-    </div>
-    <script
-  src="http://code.jquery.com/jquery-3.3.1.min.js"
-  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-  crossorigin="anonymous"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script src="/js/master.js" charset="utf-8"></script>
-  </body>
+    <head>
+        <meta charset="utf-8">
+        <title>Cat Racer</title>
+        <link rel="stylesheet" href="css/reset.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/master.css">
+    </head>
+    <body>
+        <!--Navigation Bar-->
+        <nav class="navbar navbar-expand-sm fixed-top navbar-dark">
+            <div class="container">
+                <a class="navbar-brand" href="#">Cat Racer</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Info</a>
+                        </li>
+                    </ul>
+                    <span class="navbar-text">
+                    Login
+                    </span>
+                </div>
+            </div>
+        </nav>
+        <!--Particle background-->
+        <header>
+            <canvas id="canvas" width="300" height="300"></canvas>
+        </header>
+        <!--Main content after this-->
+        <div id="textContent">
+            <div id="textSentence" class="text-left text-wrap text-break font-weight-bolder">
+                <?php
+                $sentence = explode(" ", $results["texts"]);
+                for ($i=0; $i < count($sentence); $i++) {
+                    echo "<div class='word'>";
+                    for ($ii=0; $ii < strlen($sentence[$i]); $ii++) {
+                        echo "<p class='letter'>" . $sentence[$i][$ii] . "</p>";
+                    };
+                    echo "</div>";
+                };
+                echo "<p id='textHidden'>" . $results["texts"] . "</p>";
+                ?>
+            </div>
+        </div>
+        <script src="js/master.js" charset="utf-8"></script>
+        <script src="js/particles.js" charset="utf-8"></script>
+    </body>
 </html>
--- php/utility.php
+++ php/utility.php
@@ -1,19 +1,45 @@
 <?php
-function getDbObject() {
-    $rdbms = 'mysql';
-    $host = 'localhost';
-    $db = 'cat-racer';
-    $user = 'root';
-    $pass = '';
-    $charset = 'utf8';
+    // $textVar = array();
+    //     while ($row = $result->fetch()) {
+    //         $textVar[] = $row;
+    // };
+    //
+    // foreach ($textVar as $yupyup) {
+    //         extract($yupyup);
+    //         echo $texts;
+    // };
+class RandomText {
+    // Declare database connection from utility.php
+    private function getDbObject() {
+        $rdbms = 'mysql';
+        $host = 'localhost';
+        $db = 'cat-racer';
+        $user = 'root';
+        $pass = '';
+        $charset = 'utf8';
 
-    $opt = [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES => false,
-    ];
+        $opt = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ];
 
-    $connectionString = "$rdbms:host=$host;dbname=$db;charset=$charset";
-    return new PDO($connectionString, $user, $pass, $opt);
+        $connectionString = "$rdbms:host=$host;dbname=$db;charset=$charset";
+        return new PDO($connectionString, $user, $pass, $opt);
+    }
+
+    // Selects random quote from database (texts table)
+    private function textGet() {
+    }
+    public function textQuery() {
+        $connection = $this->getDbObject();
+        $sql = "SELECT * FROM texts ORDER BY RAND() LIMIT 1";
+        $results = array();
+        foreach ($connection->query($sql) as $row) {
+            $results += $row;
+        }
+        return $results;
+    }
 }
+$CatText = new RandomText();
 ?>
